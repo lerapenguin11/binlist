cdd40de5ace770b2265b76a0275c4fa7ed12c504
Коновалова Лера <73699428+lerapenguin11@users.noreply.github.com>
2025-01-12 18:26:18 +0500

    add button

--- app/src/main/java/com/example/binlist/component/button/Button.kt
+++ app/src/main/java/com/example/binlist/component/button/Button.kt
@@ -1,7 +1,9 @@
 package com.example.binlist.component.button
 
+import androidx.compose.animation.Crossfade
 import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.heightIn
+import androidx.compose.foundation.layout.widthIn
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Button
 import androidx.compose.material3.ButtonDefaults
@@ -10,38 +12,45 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.stringResource
-import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import com.example.binlist.component.progressbar.CircularProgressBar
 import com.example.binlist.ui.theme.BinTheme
 import com.example.binlist.utils.CommonString
 
 @Composable
 fun PrimaryButton(
     variant: ButtonVariant,
-    modifier: Modifier = Modifier
+    modifier: Modifier = Modifier,
+    onClick: () -> Unit
 ) {
     Button(
         modifier = modifier
-            .heightIn(56.dp),
-        onClick = { /*TODO*/ },
+            .heightIn(56.dp)
+            .widthIn(min = 65.dp),
+        onClick = { onClick() },
         colors = ButtonDefaults.buttonColors(
             containerColor = BinTheme.colors.primary,
-            disabledContainerColor = BinTheme.colors.primary
+            disabledContainerColor = BinTheme.colors.primary,
+            disabledContentColor = Color.White
         ),
         enabled = variant == ButtonVariant.FILLED,
         shape = RoundedCornerShape(size = 4.dp),
-        contentPadding = PaddingValues(horizontal = 13.dp)
+        contentPadding = PaddingValues(horizontal = 12.dp)
     ) {
-        Text(
-            text = stringResource(CommonString.text_lookup),
-            style = BinTheme.typography.regular12,
-            color = Color.White
-        )
-    }
-}
+        Crossfade(targetState = variant, label = "") { buttonContent ->
+            when (buttonContent) {
+                ButtonVariant.FILLED -> {
+                    Text(
+                        text = stringResource(CommonString.text_lookup),
+                        style = BinTheme.typography.regular12,
+                        color = Color.White
+                    )
+                }
 
-@Preview(showBackground = true)
-@Composable
-fun PreviewPrimaryButton() {
-    PrimaryButton(variant = ButtonVariant.FILLED)
+                ButtonVariant.LOADING -> {
+                    CircularProgressBar()
+                }
+            }
+        }
+    }
 }
---
+++ app/src/main/java/com/example/binlist/component/progressbar/ProgressBar.kt
@@ -0,0 +1,23 @@
+package com.example.binlist.component.progressbar
+
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.width
+import androidx.compose.material3.CircularProgressIndicator
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.unit.dp
+
+@Composable
+fun CircularProgressBar(
+    modifier: Modifier = Modifier
+) {
+    CircularProgressIndicator(
+        modifier = modifier
+            .width(24.dp)
+            .height(24.dp),
+        color = Color.White,
+        trackColor = Color.Transparent,
+        strokeWidth = 2.dp
+    )
+}
