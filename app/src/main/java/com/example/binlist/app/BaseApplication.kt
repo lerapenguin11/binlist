8b021fb3309dc012179634fd38020a08e160c1c0
Коновалова Лера <73699428+lerapenguin11@users.noreply.github.com>
2025-01-14 16:44:18 +0500

    customization koin

---
+++ app/src/main/java/com/example/binlist/app/BaseApplication.kt
@@ -0,0 +1,29 @@
+package com.example.binlist.app
+
+import android.app.Application
+import com.example.binlist.di.appModule
+import com.example.binlist.di.dataModule
+import com.example.binlist.di.domainModule
+import org.koin.android.ext.koin.androidContext
+import org.koin.android.ext.koin.androidLogger
+import org.koin.core.context.startKoin
+import org.koin.core.logger.Level
+
+class BaseApplication : Application() {
+
+    override fun onCreate() {
+        super.onCreate()
+
+        startKoin {
+            androidLogger(Level.DEBUG)
+            androidContext(this@BaseApplication)
+            modules(
+                listOf(
+                    appModule,
+                    dataModule,
+                    domainModule
+                )
+            )
+        }
+    }
+}
---
+++ app/src/main/java/com/example/binlist/di/DomainModule.kt
@@ -0,0 +1,5 @@
+package com.example.binlist.di
+
+import org.koin.dsl.module
+
+val domainModule = module {  }
