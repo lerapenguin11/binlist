5a368b85010e18c6337aa2b5fe23273e418f97d4
Коновалова Лера <73699428+lerapenguin11@users.noreply.github.com>
2025-01-15 19:58:29 +0500

    refactoring

--- app/src/main/java/com/example/binlist/domain/usecase/GetBankInfoUseCase.kt
+++ app/src/main/java/com/example/binlist/domain/usecase/GetBankInfoUseCase.kt
@@ -1,25 +1,31 @@
 package com.example.binlist.domain.usecase
 
 import com.example.binlist.domain.model.bin.Bin
+import kotlinx.coroutines.flow.MutableSharedFlow
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.SharedFlow
 import org.koin.core.component.KoinComponent
 import org.koin.core.component.inject
 
 class GetBankInfoUseCase {
-    private val streamEventsWithQueryParam: MutableStateFlow<Bin?> =
-        MutableStateFlow(null)
+    private val streamBankInfoFromBinParam: MutableSharedFlow<Bin?> = MutableStateFlow(null)
     private var lastValue: Bin? = null
 
     fun loadBankInfo(
         bin: Bin
     ) {
-        lastValue = bin
-        streamEventsWithQueryParam.tryEmit(value = bin)
+        if (lastValue?.bin != bin.bin) {
+            lastValue = bin
+            streamBankInfoFromBinParam.tryEmit(value = bin)
+        } else {
+            lastValue?.run {
+                streamBankInfoFromBinParam.tryEmit(value = this)
+            }
+        }
     }
 
     fun trigger(): SharedFlow<Bin?> =
-        streamEventsWithQueryParam
+        streamBankInfoFromBinParam
 }
 
 class InteractorLoadBankInfo : KoinComponent {
