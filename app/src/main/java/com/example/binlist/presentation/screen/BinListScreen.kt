23103796387f1ebf0931260b49f0b9bf4afeeb87
Коновалова Лера <73699428+lerapenguin11@users.noreply.github.com>
2025-01-16 05:32:52 +0500

    refactoring code

--- app/src/main/java/com/example/binlist/presentation/screen/BinListScreen.kt
+++ app/src/main/java/com/example/binlist/presentation/screen/BinListScreen.kt
@@ -7,7 +7,6 @@ import androidx.compose.foundation.lazy.items
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.lifecycle.compose.collectAsStateWithLifecycle
